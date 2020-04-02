95d639c05dc579f07dca374b470e4ab82356ad29
Sven Thiele <sthiele78@gmail.com>
2020-04-02 20:06:32 +0200

    fix test

--- src/tests/test.py
+++ src/tests/test.py
@@ -1,13 +1,13 @@
 import subprocess
+from subprocess import PIPE
 import json
-
 
 def test_example():
     p = subprocess.run(['clingoLP', 'example_encoding.lp', 'example_instance.lp', '0',
-                        '--outf=2', '--quiet=1,0,0'], capture_output=True)
+                        '--outf=2', '--quiet=1,0,0'], stdout=PIPE, stderr=PIPE)
 
-    print(p.stdout)
-    print(p.stderr)
+    # print(p.stdout)
+    # print(p.stderr)
     out = json.loads(p.stdout)
 
     assert (out['Call'][0]['Result'] == 'SATISFIABLE')
